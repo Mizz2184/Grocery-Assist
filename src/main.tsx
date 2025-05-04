dbdc796ce7c667f7bd7b2a442ab7e0e899b5941c
Mike <mike@macbookair.mynetworksettings.com>
2025-05-04 05:27:46 -0600

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SpeedInsights } from '@vercel/speed-insights/react'
+import { Analytics } from '@vercel/analytics/react'
 
 // Ensure consistent React context by using React.StrictMode
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </React.StrictMode>
 );
